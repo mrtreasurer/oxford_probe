b30f553ee47b80ef620539700deef20170fa5bce
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2022-04-20 11:39:52 +0200

    gitignore, rename file

--- python/main.py
+++ python/main.py
@@ -1,21 +1,22 @@
 import csv
 import pathlib
 import serial
+import time
 
 from datetime import datetime
 
 
 if __name__ == "__main__":
-    arduino = serial.Serial(port='COM5', baudrate=57600, timeout=1)
+    arduino = serial.Serial(port='COM5', baudrate=57600, timeout=.1)
 
-    now = datetime.strftime(datetime.now(), "%Y%m%d-%H%M")
+    filename_time = datetime.strftime(datetime.now(), "%Y%m%d-%H%M")
     name = "probe1"
 
     data_path = pathlib.Path(__file__).parent / "data"
     if not data_path.exists():
         data_path.mkdir()
 
-    with open(f"data/{now}_{name}" , "w", newline="") as f:
+    with open(f"data/{filename_time}_{name}.csv" , "w", newline="") as f:
         csv_writer = csv.writer(f)
         arduino.flush()
         arduino.readline()
@@ -23,7 +24,7 @@ if __name__ == "__main__":
         try:
             while True:
                 line = arduino.readline().decode()
-                lst = [int(b) for b in line.split(",")]
+                lst = [time.time()] + [int(b) for b in line.split(",")]
                 csv_writer.writerow(lst)
 
         except KeyboardInterrupt:
