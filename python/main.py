a2babe4048c77d95b14b4a39e55b37c2f639c6c9
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2022-04-20 13:18:05 +0200

    blah

--- python/main.py
+++ python/main.py
@@ -7,25 +7,41 @@ from datetime import datetime
 
 
 if __name__ == "__main__":
-    arduino = serial.Serial(port='COM5', baudrate=57600, timeout=.1)
-
-    filename_time = datetime.strftime(datetime.now(), "%Y%m%d-%H%M")
-    name = "probe1"
-
     data_path = pathlib.Path(__file__).parent / "data"
     if not data_path.exists():
         data_path.mkdir()
 
-    with open(f"data/{filename_time}_{name}.csv" , "w", newline="") as f:
-        csv_writer = csv.writer(f)
+    ports = ["COM5"]
+    names = ["probe1"]
+
+    arduinos = list()
+    files = list()
+    writers = list()
+
+    for p, n in zip(ports, names):
+        arduino = serial.Serial(port=p, baudrate=57600, timeout=.1)
+        arduinos.append(arduino)
+        
+        filename_time = datetime.strftime(datetime.now(), "%Y%m%d-%H%M")
+        filename = data_path / f"{filename_time}_{n}.csv"
+        
+        f = open(filename, "w", newline="")
+
+        files.append(f)
+        writers.append(csv.writer(f))
+
         arduino.flush()
         arduino.readline()
 
-        try:
-            while True:
-                line = arduino.readline().decode()
+    try:
+        while True:
+            for arduino, csv_writer in zip(arduinos, writers):
+                line = arduino.readline()
+                print(line)
+                line = line.decode()
                 lst = [time.time()] + [int(b) for b in line.split(",")]
                 csv_writer.writerow(lst)
 
-        except KeyboardInterrupt:
-            pass
+    except KeyboardInterrupt:
+        for f in files:
+            f.close()
